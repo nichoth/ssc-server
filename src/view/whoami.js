0a639e1c55bed2a1ac75361bcbd0f276e773feef
nichoth <nichoth@gmail.com>
2021-05-26 10:09:50 -0700

    better form stuff

--- src/view/whoami.js
+++ src/view/whoami.js
@@ -11,6 +11,8 @@ var _getId = require('../get-id')
 function Whoami (props) {
     var { me, emit } = props
     const [isCreating, setCreating] = useState(false);
+    const [isSaving, setSaving] = useState(false);
+    const [keyErr, setKeyErr] = useState(null);
 
     console.log('props', props)
 
@@ -50,6 +52,39 @@ function Whoami (props) {
         console.log('create local id', keys)
         emit(evs.keys.got, { source: null, secrets: keys })
         setCreating(false)
+    }
+
+    // save the current (local) id to a server
+    function startSaving (ev) {
+        ev.preventDefault()
+        console.log('save click')
+        setSaving(true)
+    }
+
+    function saveId (ev) {
+        ev.preventDefault()
+        console.log('save the local id to a server')
+    }
+
+    function cancelSaveId (ev) {
+        setSaving(false)
+    }
+
+    function setPasteForm (ev) {
+        ev.preventDefault()
+        var _keys = ev.target.elements['key-info'].value
+        console.log('set paste form', _keys)
+        try {
+            var keys = JSON.parse(_keys)
+        } catch (err) {
+            setKeyErr(err)
+        }
+        // check the format of `keys`
+        if (!keys.public || !keys.private || !keys.id || !keys.curve) {
+            setKeyErr(new Error('Invalid key format'))
+            return
+        }
+        emit(evs.keys.got, { source: null, secrets: keys })
     }
 
     var idInfo = me ? xtend(me.secrets) : null
@@ -114,14 +149,28 @@ function Whoami (props) {
 
         <hr />
 
-        <!-- if me.source is null, lets you save the id to a server
-        b/c null means it is a local id -->
+        <!-- if me.source is null, show this. lets you save the id to a
+        server. null means it is a local id -->
         ${(me && me.secrets && !me.source) ?
             html`<div>
                 <h2>Save the current local ID to a server</h2>
-                <form>
-                    <button type="submit">save</button>
-                </form>
+                ${isSaving ?
+                    html`<form onsubmit=${saveId} onreset=${cancelSaveId}>
+                        <h2>Save this id to a server</h2>
+                        <pre>${JSON.stringify(me, null, 2)}</pre>
+                        <div class="form-group">
+                            <label for="url">URL</label>
+                            <input type="text" name="url" id="url" required />
+                        </div>
+                        <button type="reset">cancel</button>
+                        <button type="submit">submit</button>
+                    </form>` :
+                    html`<form>
+                        <button type="submit" onClick=${startSaving}>
+                            save
+                        </button>
+                    </form>`
+                }
             </div>` :
             null
         }
@@ -129,18 +178,42 @@ function Whoami (props) {
         ${!me.source ? html`<hr />` : null}
 
         <div>
-            <h2>Create a new local identity</h2>
+            <h2>Create a new identity</h2>
+            <p>Either locally or via an ID server</p>
             <button onclick=${create}>Create a new identity</button>
         </div>
 
         <hr />
 
         <!-- the login form -->
-        <form class="whoami-form" onsubmit=${getId}>
-            <h2>Use an existing identity</h2>
+        <h2>Use an existing identity</h2>
 
+        <div class="id-paste-form id-source">
+            <h3>From a local source</h3>
+            <p>This should be JSON containing your keys</p>
+            <p>For example:</p>
+            <pre>
+                ${JSON.stringify({
+                    "curve": "ed25519",
+                    "public": "123",
+                    "private": "abc",
+                    "id": "@123"
+                }, null, 2)}
+            </pre>
+            <form onsubmit=${setPasteForm}>
+                <textarea name="key-info" id="key-info"></textarea>
+                <button type="submit">set keys</button>
+            </form>
+        </div>
+
+        <form class="whoami-form" onsubmit=${getId}>
             <div class="id-source">
-                <h2>Use <code>${URL}</code> as an ID server</h2>
+                <h3>Use <code>${URL}</code> as an ID server</h3>
+
+                ${!me.source ?
+                    html`<p>This will get rid of your local ID</p>` :
+                    null
+                }
 
                 <div>
                     <label for="login-name">name</label>
