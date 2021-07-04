6d048322f9d0a00e54e873290075ab36805d72d7
nichoth <nichoth@gmail.com>
2021-07-04 12:50:13 -0700

    real test for the edit pencil

--- cypress/integration/name.js
+++ cypress/integration/name.js
@@ -2,8 +2,10 @@ var URL = 'http://localhost:8888'
 
 describe('the name field', () => {
 
-    it('name stuff', () => {
+    it('should not have a button if you dont have and id', () => {
         cy.visit(URL)
+        cy.get('button.edit-pencil')
+            .should('not.exist')
     })
 
 })
