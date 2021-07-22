52feeb961459f0035a683127ee9f403ced5289ca
nichoth <nichoth@gmail.com>
2021-07-22 10:34:10 -0700

    wip passwords

--- hash.js
+++ hash.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 var minimist = require('minimist')
+const bcrypt = require('bcrypt');
 
-function createHash (arg) {
-    return arg + ' '
+function createHash (pw) {
+    // return arg + ' '
+    const saltRounds = 10
+    return bcrypt.hash(pw, saltRounds)
+        .then(hash => {
+            console.log(hash);
+        })
 }
 
 // if (is running from the CLI)
@@ -10,10 +16,12 @@ if (require.main === module) {
     var argv = minimist(process.argv.slice(2))
     var pw = argv._[0]
     if (pw) {
-        process.stdout.end(createHash(pw))
+        createHash(pw)
+            .then(h => process.stdout.end(h))
     } else {
         process.stdin.on('data', function (d) {
-            process.stdout.end(createHash(d.toString().trim()))
+            createHash(d.toString().trim())
+                .then(h => process.stdout.end(h))
         })
     }
 
