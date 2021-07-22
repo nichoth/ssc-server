47c21f7a6c4aca2eb9bb71956a7b0b564d0198b3
nichoth <nichoth@gmail.com>
2021-07-22 09:42:58 -0700

    wip hash

--- hash.js
+++ hash.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
+var minimist = require('minimist')
 
 function createHash (arg) {
-    return 'wooooo'
+    return arg + ' '
 }
 
+// if (is running from the CLI)
 if (require.main === module) {
-    console.log(createHash())
+    var argv = minimist(process.argv.slice(2))
+    var pw = argv._[0]
+    if (pw) {
+        process.stdout.end(createHash(pw))
+    } else {
+        process.stdin.on('data', function (d) {
+            process.stdout.end(createHash(d.toString().trim()))
+        })
+    }
+
 }
 
 module.exports = createHash
