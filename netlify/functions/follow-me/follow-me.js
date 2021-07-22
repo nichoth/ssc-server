55eb378215c2194dcd752fc35560026f5f603877
nichoth <nichoth@gmail.com>
2021-07-22 10:55:11 -0700

    follow me function

--- netlify/functions/follow-me/follow-me.js
+++ netlify/functions/follow-me/follow-me.js
@@ -1,7 +1,16 @@
 require('dotenv').config()
+var pwds = require('../passwords.json')
+var hash = require('../../../hash')
 
 exports.handler = function (ev, ctx, cb) {
     console.log('file', file)
+
+    if (ev.httpMethod !== 'POST') {
+        return cb(null, {
+            statusCode: 400,
+            body: (new Error('should be a post request')).toString()
+        })
+    }
 
     // check that method is POST
 
@@ -14,8 +23,27 @@ exports.handler = function (ev, ctx, cb) {
     var req = JSON.parse(ev.body)
     console.log('got req', req)
 
+    var { password, user } = req
+
+    var ok = pwds[hash(password)]
+
+    if (ok) {
+        // in here, write to DB
+        return client.query(
+            q.Create(q.Collection('server-following'), {
+                data: { type: 'follow', contact: user }
+            })
+        )
+            .then(res => {
+                return cb(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(res.data)
+                })
+            })
+    }
+
     return cb(null, {
-        statusCode: 200,
-        body: JSON.stringify({ ok: true })
+        statusCode: 401,
+        body: (new Error('Invalid password')).toString()
     })
 }
