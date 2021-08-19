a57272dd63c03b8124ede54019a91fb9646cdf20
nichoth <nichoth@gmail.com>
2021-08-19 16:01:36 -0700

    add test for `hello`

--- cypress/integration/hello.js
+++ cypress/integration/hello.js
@@ -4,5 +4,14 @@ describe('When you dont have an identity', () => {
     it('should show the `hello` route', () => {
         cy.visit(URL + '/')
         cy.url().should('equal', URL + '/hello')
+        cy.contains('looks like you').should('exist')
+    })
+
+    it('should let you create a new id', () => {
+        cy.get('.create-id button').click()
+    })
+
+    it ('should take you to the invitation screen', () => {
+        cy.contains('need an invitation')
     })
 })
