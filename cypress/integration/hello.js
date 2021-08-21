d9ecdd98a5b3e226d2eeffb0c363e186fa6e8fc2
nichoth <nichoth@gmail.com>
2021-08-20 21:45:57 -0700

    test a good invitation

--- cypress/integration/hello.js
+++ cypress/integration/hello.js
@@ -21,11 +21,16 @@ describe('When you dont have an identity', () => {
     it('should show an error if the invitation is bad', () => {
         cy.get('.redeem input').type('foooooo')
         cy.get('.redeem button').click()
+        cy.contains('.error', 'Error: Invalid invitation')
     })
 
-    // it('should redeem an invitation', () => {
-    //     // console.log('bbbbbbb', Cypress.env('TEST_PW'))
-    //     cy.get('.redeem input').clear().type(Cypress.env('TEST_PW'))
-    //     cy.get('.redeem button').click()
-    // })
+    it('should redeem an invitation', () => {
+        cy.visit(URL + '/hello')
+        cy.get('.create-id button').click()
+        cy.get('.redeem input').clear().type(Cypress.env('TEST_PW'))
+        // console.log('bbbbbbb', Cypress.env('TEST_PW'))
+        cy.get('.redeem button').click()
+        cy.get('.shell').should('exist')
+        cy.url().should('equal', URL + '/')
+    })
 })
