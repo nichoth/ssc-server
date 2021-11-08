a49c1322d5d5959230ab1b1c21729b22f6f4599b
nichoth <nichoth@gmail.com>
2021-11-07 16:03:29 -0800

    env vars

--- script-db.js
+++ script-db.js
@@ -2,6 +2,7 @@ var faunadb = require('faunadb')
 
 var q = faunadb.query
 var client = new faunadb.Client({
-    secret: process.env.FAUNADB_SERVER_SECRET
+    secret: process.env.BRANCH ==='test' ?
+        process.env.FAUNADB_SERVER_SECRET_TEST :
+        process.env.FAUNADB_SERVER_SECRET
 })
-
