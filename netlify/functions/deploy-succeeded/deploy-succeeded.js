03034d43c8d99de1270318ba7cb42c83daa280b2
nick thomas <nichoth@gmail.com>
2022-05-02 11:13:39 -0700

    no chalk

--- netlify/functions/deploy-succeeded/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded/deploy-succeeded.js
@@ -1,4 +1,4 @@
-import chalk from 'chalk'
+// import chalk from 'chalk'
 const createDB = require('./create-db')
 
 console.log('aaaaaaaaaaaaaaaaaaa')
@@ -7,11 +7,13 @@ exports.handler = async function (ev, ctx) {
     console.log('wooooooooooooooooooooo')
     console.log('deploy success')
 
-    console.log(chalk.cyan('Creating your FaunaDB Database...\n'))
+    console.log('Creating your FaunaDB Database...\n')
+    // console.log(chalk.cyan('Creating your FaunaDB Database...\n'))
 
     // 1. Check for required enviroment variables
     if (!process.env.FAUNADB_SERVER_SECRET) {
-        console.log(chalk.yellow('Required FAUNADB_SERVER_SECRET enviroment variable not found.'))
+        console.log('Required FAUNADB_SERVER_SECRET enviroment variable not found.')
+        // console.log(chalk.yellow('Required FAUNADB_SERVER_SECRET enviroment variable not found.'))
         console.log(`Run "npm run bootstrap" to setup your database schema after
             adding an enironment variable "FAUNADB_SERVER_SECRET"`)
         process.exit(1)
@@ -19,6 +21,7 @@ exports.handler = async function (ev, ctx) {
 
     // Has var. Do the thing
     createDB(process.env.FAUNADB_SERVER_SECRET).then(() => {
-        console.log(chalk.green('Fauna Database schema has been created'))
+        console.log('Fauna Database schema has been created')
+        // console.log(chalk.green('Fauna Database schema has been created'))
     })
 }
