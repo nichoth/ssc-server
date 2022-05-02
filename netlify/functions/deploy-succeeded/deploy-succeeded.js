44487918579c747a30089c8a0e12d38bdd207a06
nick thomas <nichoth@gmail.com>
2022-05-01 19:46:15 -0700

    WIP bootstrap script

--- netlify/functions/deploy-succeeded/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded/deploy-succeeded.js
@@ -1,8 +1,30 @@
+// var faunadb = require('faunadb')
+// var q = faunadb.query
+const createDB = require('./create-db')
+
 console.log('aaaaaaaaaaaaaaaaaaa')
 
 exports.handler = async function (ev, ctx) {
     console.log('wooooooooooooooooooooo')
     console.log('deploy success')
-    console.log('*ev*', ev)
-    console.log('*ctx*', ctx)
+
+    console.log(chalk.cyan('Creating your FaunaDB Database...\n'))
+
+    // 1. Check for required enviroment variables
+    if (!process.env.FAUNADB_SERVER_SECRET) {
+        console.log(chalk.yellow('Required FAUNADB_SERVER_SECRET enviroment variable not found.'))
+        console.log(`Run "npm run bootstrap" to setup your database schema after
+            adding an enironment variable "FAUNADB_SERVER_SECRET"`)
+        process.exit(1)
+    }
+
+    // Has var. Do the thing
+    createFaunaDB(process.env.FAUNADB_SERVER_SECRET).then(() => {
+        console.log(chalk.green('Fauna Database schema has been created'))
+    })
+
+    function createFaunaDB (key) {
+        console.log('Create the fauna database schema!')
+        return createDB(key)
+    }
 }
