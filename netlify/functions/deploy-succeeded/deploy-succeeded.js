a3cb8b51bf38ba94844a72ff9f614dafa2f09601
nick thomas <nichoth@gmail.com>
2022-05-02 14:24:48 -0700

    rm obsolete code

--- netlify/functions/deploy-succeeded/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded/deploy-succeeded.js
@@ -1,4 +1,3 @@
-// import chalk from 'chalk'
 const createDB = require('./create-db')
 
 exports.handler = function (ev, ctx) {
@@ -7,7 +6,6 @@ exports.handler = function (ev, ctx) {
     // 1. Check for required enviroment variables
     if (!process.env.FAUNADB_SERVER_SECRET) {
         console.log('Required FAUNADB_SERVER_SECRET enviroment variable not found.')
-        // console.log(chalk.yellow('Required FAUNADB_SERVER_SECRET enviroment variable not found.'))
         console.log(`Run "npm run bootstrap" to setup your database schema after
             adding an enironment variable "FAUNADB_SERVER_SECRET"`)
         process.exit(1)
@@ -16,7 +14,6 @@ exports.handler = function (ev, ctx) {
     // Has var. Do the thing
     return createDB(process.env.FAUNADB_SERVER_SECRET).then(() => {
         console.log('Fauna Database schema has been created')
-        // console.log(chalk.green('Fauna Database schema has been created'))
     }).catch(err => {
         console.log('*oh no*', err)
     })
