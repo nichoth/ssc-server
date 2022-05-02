8a047e6cbdb4d4672e2c029d3848079a4fe016d2
nick thomas <nichoth@gmail.com>
2022-05-02 13:29:45 -0700

    try it as a bg function

---
+++ netlify/functions/deploy-succeeded-background/deploy-succeeded-background.js
@@ -0,0 +1,31 @@
+// import chalk from 'chalk'
+const createDB = require('../deploy-succeeded/create-db')
+
+console.log('aaaaaaaaaaaaaaaaaaa')
+
+exports.handler = async function (ev, ctx) {
+    console.log('wooooooooooooooooooooo')
+    console.log('deploy success')
+
+    console.log('Creating your FaunaDB Database...\n')
+    // console.log(chalk.cyan('Creating your FaunaDB Database...\n'))
+
+    // 1. Check for required enviroment variables
+    if (!process.env.FAUNADB_SERVER_SECRET) {
+        console.log('Required FAUNADB_SERVER_SECRET enviroment variable not found.')
+        // console.log(chalk.yellow('Required FAUNADB_SERVER_SECRET enviroment variable not found.'))
+        console.log(`Run "npm run bootstrap" to setup your database schema after
+            adding an enironment variable "FAUNADB_SERVER_SECRET"`)
+        process.exit(1)
+    }
+
+    // Has var. Do the thing
+    console.log('has the env var')
+    createDB(process.env.FAUNADB_SERVER_SECRET).then(() => {
+        console.log('Fauna Database schema has been created')
+        // console.log(chalk.green('Fauna Database schema has been created'))
+    }).catch(err => {
+        console.log('*oh no*', err)
+    })
+}
+
