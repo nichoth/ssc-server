f965dd2370087867a4b2bccf8df74cc8b83243be
nick thomas <nichoth@gmail.com>
2022-05-22 21:04:12 -0700

    fix alternate test

--- test/alternate.js
+++ test/alternate.js
@@ -4,8 +4,56 @@ const path = require('path')
 const ssc = require('@nichoth/ssc-lambda')
 const test = require('tape')
 const fs = require('fs')
+const onExit = require('signal-exit')
+const setup = require('./setup')
 const { admins } = require('../src/config.json')
 const base = 'http://localhost:8888'
+
+if (require.main === module) {
+    var keys
+    var ntl
+    var did
+
+    test('setup', function (t) {
+        setup(t.test, (netlify) => {
+            ntl = netlify
+
+            onExit(() => {
+                ntl.kill('SIGINT')
+            })
+
+            ssc.createKeys().then(user => {
+                keys = user.keys
+                ssc.exportKeys(user.keys).then(exported => {
+                    // need to write this did to config.admins
+                    const _did = ssc.publicKeyToDid(exported.public)
+                    did = _did
+                    console.log('*did*', _did)
+                    const configPath = path.resolve(__dirname, '..', 'src',
+                        'config.json')
+                    admins.push({ did })
+
+                    fs.writeFileSync(configPath, JSON.stringify({
+                        admins
+                    }, null, 2))
+
+
+                    t.end()
+                })
+            })
+        })
+    })
+
+    test('alternates', t => {
+        alt(t.test, keys, did)
+        t.end()
+    })
+
+    test('all done', function (t) {
+        ntl.kill()
+        t.end()
+    })
+}
 
 module.exports = alt
 
@@ -23,6 +71,13 @@ function alt (test, keys, did) {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(msg)
             }).then(res => {
+                if (!res.ok) {
+                    res.text().then(text => {
+                        console.log('errrrrrrrrrrr', text)
+                        t.fail()
+                        return t.end()
+                    })
+                }
                 return res.json()
             }).then(res => {
                 t.equal(res.key, ssc.getId(res.value),
