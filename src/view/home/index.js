7e4d3d72f9b1d3e6c3c895cafe0469e4596d9b97
nick thomas <nichoth@gmail.com>
2022-06-24 12:53:28 -0700

    WIP - show user info under posts

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -61,6 +61,12 @@ function Home (props) {
                         <img src=${url} />
                         <p>${post.value.content.text + ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '}</p>
                     </a>
+
+                    <div class="user-info">
+                        <hr />
+
+                        ${post.value.author}
+                    </div>
                 </li>`
             })}</ul>` :
             null
