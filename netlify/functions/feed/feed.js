b2c2e1a4ca03f60fbbc9660b428468406846c769
nick thomas <nichoth@gmail.com>
2022-06-28 09:24:03 -0700

    setup for by name query

--- netlify/functions/feed/feed.js
+++ netlify/functions/feed/feed.js
@@ -12,28 +12,39 @@ exports.handler = function (ev, ctx) {
         }
     }
 
-    const did = ev.queryStringParameters.did
+    const { did, username } = ev.queryStringParameters
 
-    return client.query(
-        q.Map(
-            q.Paginate(
-                q.Reverse( q.Match(q.Index('post_by_author'), did) )
-            ),
-            q.Lambda( 'post', q.Get(q.Var('post')) )
-        )
-    ).then(res => {
-        return {
-            statusCode: 200,
-            body: JSON.stringify(res.data.map(doc => {
-                // this is b/c the DB I think strips out `null`
-                // values from the message objects
-                return Object.assign(doc.data, {
-                    value: Object.assign(doc.data.value, {
-                        previous: doc.data.value.previous || null
+    if (did) {
+        return client.query(
+            q.Map(
+                q.Paginate(
+                    q.Reverse( q.Match(q.Index('post_by_author'), did) )
+                ),
+                q.Lambda( 'post', q.Get(q.Var('post')) )
+            )
+        ).then(res => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(res.data.map(doc => {
+                    // this is b/c the DB I think strips out `null`
+                    // values from the message objects
+                    return Object.assign(doc.data, {
+                        value: Object.assign(doc.data.value, {
+                            previous: doc.data.value.previous || null
+                        })
                     })
-                })
-            }))
-        }
-    })
+                }))
+            }
+        })
+    }
 
+    if (username) {
+        // return query by name
+    }
+
+    // no username or did
+    return {
+        statusCode: 422,
+        body: 'missing query param'
+    }
 }
