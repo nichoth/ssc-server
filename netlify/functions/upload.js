a53e9399dc5aa6b69009c04d083496e52b69a69d
nick thomas <nichoth@gmail.com>
2022-07-02 13:02:39 -0700

    rm old dep

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -1,4 +1,3 @@
-// let cloudinary = require("cloudinary").v2;
 const BlobStore = require('@nichoth/blob-store/cloudinary')
 const blobStore = BlobStore({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -6,28 +5,8 @@ const blobStore = BlobStore({
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-// cloudinary.config({ 
-//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET
-// })
-
-// function upload (file, hash) {
 function upload (file) {
-    // var slugifiedHash = encodeURIComponent('' + hash)
-
     return blobStore.write(file)
-
-    // return new Promise(function (resolve, reject) {
-    //     cloudinary.uploader.upload(file, {
-    //         public_id: slugifiedHash,
-    //         resource_type: 'auto',
-    //         overwrite: true
-    //     }, function (err, res) {
-    //         if (err) return reject(err)
-    //         resolve(res)
-    //     })
-    // })
 }
 
 module.exports = upload
